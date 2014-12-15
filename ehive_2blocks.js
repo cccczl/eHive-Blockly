29e352f9d4935130af054ec0b79e3f955ba63c95
Leo Gordon <lg4@ebi.ac.uk>
2014-12-15 16:38:04 +0000

    support for dataflow and semaphores, but not yet non-analysis dataflow targets

--- ehive_2blocks.js
+++ ehive_2blocks.js
@@ -13,7 +13,8 @@ Blockly.PipeConfig.toWorkspace = function(json_text, workspace) {
 
 Blockly.PipeConfig.toWorkspace.pipeline = function(pipeline_node, workspace) {
 
-    var pipeline_wide_parameters = pipeline_node['pipeline_wide_parameters'];
+    var pipeline_wide_parameters    = pipeline_node['pipeline_wide_parameters'];
+    var pipeline_analyses           = pipeline_node['pipeline_analyses'] || [];
 
     var targetBlock = Blockly.Block.obtain(workspace, 'pipeline');
     targetBlock.initSvg();
@@ -23,24 +24,18 @@ Blockly.PipeConfig.toWorkspace.pipeline = function(pipeline_node, workspace) {
         Blockly.PipeConfig.toWorkspace.dictionary(pipeline_wide_parameters, targetBlock.getInput('parameters').connection);
     }
 
-    var pipeline_analyses = pipeline_node['pipeline_analyses'] || [];
-    var parentConnection = targetBlock.nextConnection;
-    for(var i in pipeline_analyses) {
-        var analysis_node = pipeline_analyses[i];
 
-        var analysisBlock = Blockly.PipeConfig.toWorkspace.analysis(analysis_node, parentConnection);
-        parentConnection = analysisBlock.nextConnection;
-    }
+    Blockly.PipeConfig.toWorkspace.analyses(pipeline_analyses, targetBlock.nextConnection);
 };
 
 
 Blockly.PipeConfig.toWorkspace.dictionary = function(dictionary_node, parentConnection) {
+
     var targetBlock = Blockly.Block.obtain(parentConnection.sourceBlock_.workspace, 'dictionary');
+
     targetBlock.initSvg();
     targetBlock.render();
-
-    var childConnection = targetBlock.outputConnection;
-    parentConnection.connect(childConnection);
+    parentConnection.connect( targetBlock.outputConnection );   // establish the horizontal link
 
     var i=0;
     for(var key in dictionary_node) {
@@ -54,12 +49,16 @@ Blockly.PipeConfig.toWorkspace.dictionary = function(dictionary_node, parentConn
 };
 
 
-Blockly.PipeConfig.toWorkspace.analysis = function(analysis_node, parentConnection) {
+Blockly.PipeConfig.toWorkspace.analyses = function(analyses, parentConnection) {
+
+    if(analyses.length == 0) return;
+
+    var analysis_node       = analyses.shift();
 
     var analysis_name       = analysis_node['analysis_name'];
     var module              = analysis_node['module'];
     var analysis_parameters = analysis_node['analysis_parameters'];
-    var dataflows           = analysis_node['dataflows'];
+    var dataflows           = analysis_node['dataflows'] || [];
 
     var targetBlock = Blockly.Block.obtain(parentConnection.sourceBlock_.workspace, 'analysis');
 
@@ -73,8 +72,126 @@ Blockly.PipeConfig.toWorkspace.analysis = function(analysis_node, parentConnecti
         Blockly.PipeConfig.toWorkspace.dictionary(analysis_parameters, targetBlock.getInput('parameters').connection);
     }
 
-    var childConnection = targetBlock.previousConnection;
-    parentConnection.connect(childConnection);
+    parentConnection.connect( targetBlock.previousConnection ); // establish the vertical link
+
+    var dataflows_connect_here = targetBlock.getInput('dataflows').connection;
+
+    for(var i in dataflows) {
+        var dataflow_node   = dataflows[i];
+        var template        = dataflow_node['template'];
+
+        var isBackbone  = ! dataflow_node.hasOwnProperty('branch_number');
+        var isSemaphore =   dataflow_node.hasOwnProperty('semaphore_fan');
+
+        if(isBackbone) {        // (links DOWN)
+
+            if(template) {
+                Blockly.PipeConfig.toWorkspace.dictionary(template, targetBlock.getInput('template').connection);
+            }
+
+            if(isSemaphore) {   // semaphored_dataflow:
+                Blockly.PipeConfig.toWorkspace.semaphored_dataflow(dataflow_node, analyses, targetBlock.nextConnection);
+            } else {            // next analysis:
+                Blockly.PipeConfig.toWorkspace.analyses(analyses, targetBlock.nextConnection);
+            }
+        } else {                // (links to the RIGHT)
+            if(isSemaphore) {   // extra_semaphore:
+                dataflows_connect_here = Blockly.PipeConfig.toWorkspace.extra_semaphore(dataflow_node, dataflows_connect_here);
+            } else {            // dataflow_rule:
+                dataflows_connect_here = Blockly.PipeConfig.toWorkspace.dataflow_rule(dataflow_node, dataflows_connect_here);
+            }
+        }
+    }
 
     return targetBlock;
 };
+
+
+Blockly.PipeConfig.toWorkspace.dataflow_rule = function(dataflow_node, parentConnection) {
+
+    var branch_number       = dataflow_node['branch_number'];
+    var template            = dataflow_node['template'];
+    var target_chain        = dataflow_node['target_chain'];
+    var target_ref          = dataflow_node['target_ref'];
+
+    var targetBlock = Blockly.Block.obtain(parentConnection.sourceBlock_.workspace, 'dataflow_rule');
+
+    targetBlock.setFieldValue( branch_number, 'branch_number');
+
+    targetBlock.initSvg();
+    targetBlock.render();
+    parentConnection.connect( targetBlock.outputConnection );   // establish the horizontal link
+
+    if(template) {
+        Blockly.PipeConfig.toWorkspace.dictionary(template, targetBlock.getInput('template').connection);
+    }
+
+    if(target_chain) {
+        Blockly.PipeConfig.toWorkspace.analyses(target_chain, targetBlock.nextConnection);
+    } else if(target_ref) {
+        console.log('TODO: target_refs not implemented yet');
+    } else {
+        console.log('ERROR: unexpected lack of target');
+    }
+
+    return targetBlock.getInput('more dataflows').connection;
+};
+
+
+Blockly.PipeConfig.toWorkspace.semaphored_dataflow = function(dataflow_node, analyses, parentConnection) {
+
+    if(analyses.length == 0) return;
+
+    var targetBlock = Blockly.Block.obtain(parentConnection.sourceBlock_.workspace, 'semaphored_dataflow');
+
+    targetBlock.initSvg();
+    targetBlock.render();
+    parentConnection.connect( targetBlock.previousConnection ); // establish the vertical link
+
+    var semaphore_fan           = dataflow_node['semaphore_fan'];
+    var semaflows_connect_here  = targetBlock.getInput('fan').connection;
+
+    for(var i in semaphore_fan) {
+        var semaflow_node   = semaphore_fan[i];
+
+        semaflows_connect_here = Blockly.PipeConfig.toWorkspace.dataflow_rule(semaflow_node, semaflows_connect_here );
+    }
+
+    Blockly.PipeConfig.toWorkspace.analyses(analyses, targetBlock.nextConnection);
+};
+
+
+Blockly.PipeConfig.toWorkspace.extra_semaphore = function(dataflow_node, parentConnection) {
+
+    var branch_number       = dataflow_node['branch_number'];
+    var template            = dataflow_node['template'];
+    var target_chain        = dataflow_node['target_chain'];
+
+    if(target_chain.length == 0) return;
+
+    var targetBlock = Blockly.Block.obtain(parentConnection.sourceBlock_.workspace, 'extra_semaphore');
+
+    targetBlock.setFieldValue( branch_number, 'branch_number');
+
+    targetBlock.initSvg();
+    targetBlock.render();
+    parentConnection.connect( targetBlock.outputConnection );   // establish the horizontal link
+
+    if(template) {
+        Blockly.PipeConfig.toWorkspace.dictionary(template, targetBlock.getInput('template').connection);
+    }
+
+    var semaphore_fan           = dataflow_node['semaphore_fan'];
+    var semaflows_connect_here  = targetBlock.getInput('fan').connection;
+
+    for(var i in semaphore_fan) {
+        var semaflow_node   = semaphore_fan[i];
+
+        semaflows_connect_here = Blockly.PipeConfig.toWorkspace.dataflow_rule(semaflow_node, semaflows_connect_here );
+    }
+
+    Blockly.PipeConfig.toWorkspace.analyses(target_chain, targetBlock.nextConnection);
+
+    return targetBlock.getInput('more dataflows').connection;
+};
+
